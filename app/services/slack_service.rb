7a1d8a7deecdf07ab42d4aac3f717d35590cefd0
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-04-07 21:06:10 +0300

    Slack interaction implementation

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -1,7 +1,12 @@
+# This class implements all required interaction with Slack service
+# Some methods are used during new member onboarding, others are required
+# for new project kick-off.
 class SlackService
-  def initialize
+  def initialize(token)
+    @token = token
   end
 
+  # We invite new registred member to our Slack
   def invite(email, first_name, last_name)
     SlackIntegration::InviteUser.new(
       email:      email,
@@ -11,18 +16,44 @@ class SlackService
     ).call
   end
 
+  # When the new idea is submitted we notify developers through Slack
   def post_to_channel(channel, message)
+    client.chat_postMessage(channel: channel, text: message, as_user: false, username: 'Idea Bot')
   end
 
-  def invite_to_channel(channel, email)
+  # At the moment of new project kick-off we invite all interested members to its channel
+  def invite_to_channel(channel_name, email)
+    user = user_by_email(email)
+    fail SlackIntegration::FailedApiCallException, "User with email '#{email}' was not found" unless user
+    channel = channel_by_name(channel_name)
+    fail SlackIntegration::FailedApiCallException, "Channel with name '#{channel}' was not found" unless channel
+    client.conversations_invite(channel: channel['id'], users: user['id'])
   end
 
+  # For each new project we create a separate channel
   def create_channel(channel_name)
+    client.conversations_create(name: channel_name, is_private: false)
   end
 
   private
 
   def token
-    ENV['SLACK_TOKEN']
+    @token
+  end
+
+  def client
+    @client ||= Slack::Web::Client.new(token: token)
+  end
+
+  def channel_by_name(channel)
+    client.conversations_list['channels'].find do |entry|
+      entry['name'] == channel
+    end
+  end
+
+  def user_by_email(email)
+    client.users_list['members'].find do |entry|
+      entry['profile']['email'] == email
+    end
   end
 end
