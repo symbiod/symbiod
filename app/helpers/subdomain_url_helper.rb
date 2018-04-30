cee3d91ea4e3358aa778a1dfafecae57c251b736
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-04-30 20:56:41 +0300

    Fix rubocop offences

--- app/helpers/subdomain_url_helper.rb
+++ app/helpers/subdomain_url_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Provides methods for dealing with multidomain url helpers
 module SubdomainUrlHelper
   def nav_link(title, url, subdomain: 'www')
     is_active = request.subdomains.include?(subdomain)
