83e1a1e4738ae2545a5672da1bfff5579ef4b269
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-04-30 22:12:20 +0300

    Decrease sidekiq connection pool size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], size: 7 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], size: 1 }
 end
