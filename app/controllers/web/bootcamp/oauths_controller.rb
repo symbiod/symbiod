c0953c7d89111e21e46ff197fa02243af9740507
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-05-01 13:15:30 +0300

    Validate github profile information.
    We do not allow to sign up users, who don't have enough information
    at their github profile.
    First of all they should fill their public profile, according our
    guidelines, and then apply as members of bootcamp.

--- app/controllers/web/bootcamp/oauths_controller.rb
+++ app/controllers/web/bootcamp/oauths_controller.rb
@@ -9,33 +9,50 @@ module Web
       skip_before_action :require_login, raise: false
 
       def oauth
-        login_at(provider)
+        login_at(provider_name)
       end
 
       def callback
         sign_in || sign_up
-        redirect_back_or_to(root_url(subdomain: 'www'), notice: t('landing.success_login', provider: provider.titleize))
       end
 
       private
 
       def sign_in
-        @user = login_from(provider)
+        @user = login_from(provider_name)
+        redirect_back_or_to(root_url(subdomain: 'www'), notice: t('landing.success_login')) if @user
       end
 
       def sign_up
-        @user = create_from(provider)
+        if validate_github_profile
+          start_onboarding
+          redirect_back_or_to(root_url(subdomain: 'www'), notice: t('landing.success_login'))
+        else
+          redirect_to bootcamp_root_url, alert: t('bootcamp.landing.unfilled_github_email')
+        end
+      end
+
+      def start_onboarding
+        @user = create_from(provider_name)
         reset_session
         auto_login(@user)
         Ops::Developer::Onboarding.call(user: @user)
+      end
+
+      def validate_github_profile
+        provider_data['email'].present?
       end
 
       def auth_params
         params.permit(:code, :provider, :subdomain)
       end
 
-      def provider
+      def provider_name
         auth_params[:provider]
+      end
+
+      def provider_data
+        @user_hash[:user_info]
       end
     end
   end
