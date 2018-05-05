e54745fc4299f363406252c40a78207462967e6d
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-05-05 11:05:23 +0300

    Fix rubocop offences

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# This endpoint is used to check if the app is alive
 class PingController < ApplicationController
   def index
     head :ok, content_type: 'text/html'
