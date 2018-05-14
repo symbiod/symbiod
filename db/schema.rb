98cff6aa21fe2eba636b8580aac76a22c682026c
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-05-14 21:21:48 +0300

    Add not_null to TestTask#title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_095047) do
+ActiveRecord::Schema.define(version: 2018_05_14_181710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_095047) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
+    t.string "title", null: false
   end
 
   create_table "ideas", force: :cascade do |t|
