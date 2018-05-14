2e2abff16b07e1def645da0a9bf0c7d111b45aa7
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-05-14 21:05:23 +0300

    Revert bad migration

--- db/migrate/20180514095047_add_title_to_test_task.rb
+++ db/migrate/20180514095047_add_title_to_test_task.rb
@@ -1,5 +1,5 @@
 class AddTitleToTestTask < ActiveRecord::Migration[5.2]
   def change
-    add_column :developer_test_tasks, :title, :string, null: false
+    add_column :developer_test_tasks, :title, :string
   end
 end
