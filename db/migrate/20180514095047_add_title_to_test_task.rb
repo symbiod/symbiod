4672f850e83632ed9320f679a00d0da7036dafb9
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-05-14 19:53:38 +0300

    (#61) add spec validates title, add null false to column

--- db/migrate/20180514095047_add_title_to_test_task.rb
+++ db/migrate/20180514095047_add_title_to_test_task.rb
@@ -1,5 +1,5 @@
 class AddTitleToTestTask < ActiveRecord::Migration[5.2]
   def change
-    add_column :developer_test_tasks, :title, :string
+    add_column :developer_test_tasks, :title, :string, null: false
   end
 end
