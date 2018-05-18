05b9b03994773ed689cc6ce3f49cbf1cc8ce6e6a
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-05-19 00:02:16 +0300

    Fix infinite redirect loop

--- app/policies/bootcamp/wizard/screening_policy.rb
+++ app/policies/bootcamp/wizard/screening_policy.rb
@@ -2,7 +2,7 @@ module Bootcamp
   module Wizard
     class ScreeningPolicy < ApplicationPolicy
       def edit?
-        user&.profile_completed?
+        user&.profile_completed? || user&.screening_completed?
       end
     end
   end
