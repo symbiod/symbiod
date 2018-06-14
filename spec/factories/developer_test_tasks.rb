d7e65e3d0817f1a8b813d029a3607766d511e32e
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-06-14 21:37:15 +0300

    Fix flaky test_task factory

--- spec/factories/developer_test_tasks.rb
+++ spec/factories/developer_test_tasks.rb
@@ -2,12 +2,20 @@
 
 FactoryBot.define do
   factory :developer_test_task, class: 'Developer::TestTask' do
-    title { "#{Faker::VForVendetta.quote} #{Time.now.to_f}" }
+    title { Faker::VForVendetta.quote }
     position { %w[1 2].sample }
     description { Faker::VForVendetta.speech }
     state 'active'
     skill
     role
+
+    trait :first_position do
+      position 1
+    end
+
+    trait :second_position do
+      position 2
+    end
 
     trait :disabled do
       state 'disabled'
