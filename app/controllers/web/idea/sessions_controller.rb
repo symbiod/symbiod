03f1e5849c644b342afd8d1f44e8a2b928624001
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-06-15 22:43:18 +0300

    Password sign in refactoring

---
+++ app/controllers/web/idea/sessions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Web
+  module Idea
+    # Allows to sign in as author or staff by email/password
+    class SessionsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
+      def new
+        @user = User.new
+      end
+
+      def create
+        if (@user = login(resource_params[:email], resource_params[:password]))
+          redirect_back_or_to(root_landing_url, notice: t('landing.success_login', email: @user.email))
+        else
+          flash.now[:alert] = t('landing.failed_login')
+          render action: 'new'
+        end
+      end
+
+      private
+
+      def resource_params
+        params.require(:idea_login).permit(:email, :password)
+      end
+    end
+  end
+end
---
+++ app/controllers/web/sign_outs_controller.rb
@@ -0,0 +1,15 @@
+module Web
+  # This controller is located in root namespace to allow any kind of user
+  # to sighn out from the system, since we have different sign in logic in
+  # different namespaces
+  class SignOutsController < ApplicationController
+    def destroy
+      logout
+      reset_sorcery_session
+      # NOTICE: For some reason Sorcery does not sign out properly
+      # So we need to manualy nullify current user variable
+      @current_user = nil
+      redirect_back_or_to(root_landing_url, notice: t('landing.logout'))
+    end
+  end
+end
