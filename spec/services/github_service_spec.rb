d4ab00088e7def719eef5fd7f86543c24b412d99
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-06-17 18:03:04 +0300

    fix specs

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -114,31 +114,35 @@ describe GithubService do
     end
   end
 
-  describe '#search_users' do
+  describe '#username_by_email' do
+    before { github_response_template }
+
     context 'found user by email' do
-      before { github_response_template }
       let(:email) { 'opensource@howtohireme.ru' }
-      let(:request) do
+      let(:response) do
         { total_count: 1,
           incomplete_results: false,
           items: [{ login: 'reabiliti',
                     id: 30_253_042,
                     score: 16.607306 }] }
       end
+      let(:result) { 'reabiliti' }
+      before { allow(client).to receive(:search_users).with(email).and_return(response) }
 
       it 'query generation' do
-        allow(client).to receive(:search_users).with(email).and_return(request)
         subject.username_by_email(email)
       end
 
       it 'does not raise error' do
-        allow(client).to receive(:search_users).with(email).and_return(request)
         expect { subject.username_by_email(email) }.not_to raise_error
+      end
+
+      it 'return user name' do
+        expect(subject.username_by_email(email)).to eq result
       end
     end
 
     context 'user not found by email' do
-      before { github_response_template }
       let(:email) { 'not-opensource@howtohireme.ru' }
       let(:request) do
         { total_count: 0,
