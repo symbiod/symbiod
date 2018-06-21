237168dbd2ac36acb45051626021e3a99144d5fa
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-06-21 22:22:07 +0300

    (#206) add spec to screening start

--- spec/operations/ops/developer/screening/start_spec.rb
+++ spec/operations/ops/developer/screening/start_spec.rb
@@ -4,5 +4,14 @@ require 'rails_helper'
 
 describe Ops::Developer::Screening::Start do
   describe '#call' do
+    subject { described_class }
+    let(:user) { create(:user, :developer) }
+    let(:positions) { Ops::Developer::Screening::Start::NUMBER_OF_ASSIGNED_TEST_TASKS }
+    let(:params) { { user: user, positions: positions } }
+
+    it 'start test task assignments' do
+      expect(Ops::Developer::AssignTestTasks).to receive(:call).with(user: user, positions: positions)
+      subject.call(params)
+    end
   end
 end
