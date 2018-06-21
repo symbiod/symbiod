afa512cc742fc11b0711673e3d6e6de6927dc2b0
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-06-21 17:13:56 +0300

    (#206) refactoring test task assignments at start screening

--- app/operations/ops/developer/assign_test_tasks.rb
+++ app/operations/ops/developer/assign_test_tasks.rb
@@ -4,19 +4,26 @@ module Ops
   module Developer
     # Get random test task depending on skill and position
     class AssignTestTasks < BaseOperation
-
-      step random_test_tasks!
+      step :create_test_task_assignments!
 
       private
 
-      def random_test_tasks!(_ctx, user:, positions:, **)
-        (1..positions).each do |position|
-          ::Developer::TestTask.active.where(
-            position: position,
-            role_id: user.role_ids.first,
-            skill_id: user.primary_skill.id
-          ).sample
+      def create_test_task_assignments!(_ctx, user:, positions:, **)
+        @user = user
+        @positions = positions
+        test_tasks.each do |test_task|
+          user.test_task_assignments.create!(test_task: test_task)
         end
+      end
+
+      def test_tasks
+        (1..@positions).inject([]) do |test_tasks, position|
+          test_tasks << ::Developer::TestTask.active.where(
+            position: position,
+            role_id: @user.role_ids.first,
+            skill_id: @user.primary_skill.id
+          ).sample
+        end.compact
       end
     end
   end
