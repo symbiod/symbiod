35a6d7e86b021a217989b54637dd6e915c75c58d
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-06-23 12:18:37 +0300

    (#232) migrating settings to environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true }
+  config.action_mailer.default_url_options = { host: 'givemepoc.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
