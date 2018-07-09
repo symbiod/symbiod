fbcb6718af135975b4c1e88c34a16cbf68a5eb47
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-07-09 20:30:18 +0300

    (#296) add counter on main page

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 module Web
+  # Controller landing page
   class HomeController < ApplicationController
     def index
+      @developers_size = User.with_role(:developer).size
+      @mentors_size = User.with_role(:mentor).size
+      @projects_size = Project.all.size
     end
   end
 end
