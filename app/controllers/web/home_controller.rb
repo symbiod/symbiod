17c4381e8a8c01e6f47457f610bc4c9f85fbdd5e
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-07-20 01:00:12 +0300

    changes after review

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -4,9 +4,9 @@ module Web
   # Controller landing page
   class HomeController < ApplicationController
     def index
-      @developers_size = User.with_role(:developer).size
-      @mentors_size = User.with_role(:mentor).size
-      @projects_size = Project.all.size
+      @developers_size = User.with_role(:developer).count
+      @mentors_size = User.with_role(:mentor).count
+      @projects_size = Project.all.count
     end
   end
 end
