44f1398be394761430336d6b666ca7a504bc1c88
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-07-21 10:36:32 +0300

    add github_link helper, edit profile and add spec

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 # Method to redering date registration of user
-module DateHelper
+module ProfileHelper
   def human_date(user)
     user.created_at.strftime('%d-%m-%Y')
+  end
+
+  def github_link(user)
+    link_to user.github, "https://github.com/#{user.github}", target: :_blank
   end
 end
---
+++ spec/helpers/profile_helper_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProfileHelper do
+  subject(:user) { create(:user) }
+
+  describe 'human_date' do
+    it { expect(human_date(user)).to eq user.created_at.strftime('%d-%m-%Y') }
+  end
+
+  describe 'github_link' do
+    it { expect(github_link(user))
+           .to eq "<a target=\"_blank\" href=\"https://github.com/#{user.github}\">
+                   https://github.com/#{user.github}</a>" }
+  end
+end
