374594c47430a57c3e54ac3133a7a0ae70248ecd
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-07-22 18:39:18 +0300

    Fix broken parts

--- app/models/developer/test_task.rb
+++ app/models/developer/test_task.rb
@@ -3,12 +3,9 @@
 module Developer
   # Represents the task description, that should be solved by developer
   class TestTask < ApplicationRecord
-    # TODO: delete after deploy
-    self.ignored_columns = %w[role_id]
-
     include AASM
 
-    validates :title, :position, :skill_id, presence: true
+    validates :title, :position, :skill_id, :role_name, presence: true
     validates :description, presence: true, length: { minimum: 50 }
 
     has_many :test_task_assignments, class_name: 'Developer::TestTaskAssignment', foreign_key: 'test_task_id'
--- spec/models/developer/test_task_spec.rb
+++ spec/models/developer/test_task_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Developer::TestTask, type: :model do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :skill_id }
   it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_presence_of :role_name }
   it { is_expected.to validate_length_of(:description).is_at_least(50) }
 
   it { is_expected.to have_many(:test_task_assignments) }
