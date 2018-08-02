e907fd75169d16b5b2d1d5c787a02b780b8b60a9
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-08-02 23:22:22 +0300

    custom 404 page without tests

--- config/routes.rb
+++ config/routes.rb
@@ -78,5 +78,6 @@ Rails.application.routes.draw do
   end
 
   get '/ping', to: 'ping#index'
+  match '/404' => 'errors#error404', via: [ :get, :post, :patch, :delete ]
   root to: 'ping#redirect'
 end
