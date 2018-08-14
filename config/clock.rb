ee4c576def44940588e0a044f0ee2f13a1e2c956
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-08-14 22:51:10 +0300

    transfer clockwork file

--- config/clock.rb
+++ config/clock.rb
@@ -3,7 +3,7 @@ require_relative '../config/boot'
 require_relative '../config/environment'
 
 module Clockwork
-  every(3.day, 'SendFollowupJob'){
+  every(3.day, 'SendFollowupJob', at: '10:00'){
     Developer::SendFollowupJob.perform_later
   }
 end
