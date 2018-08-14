ca9e218011c83290c7e5633f33a992dc81467959
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-08-14 15:56:34 +0300

    add screening uncomplited query

---
+++ app/queries/users/screening_uncompleted_notification_users_query.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Users
+  # Allows to find users, who should be notified about new applicant.
+  # We should notify all staff members.
+  # Additionally we notify mentors, who has the same specialization, as user.
+  class ScreeningUncompletedNotificationUsersQuery
+    def call
+      User
+        .joins(:roles)
+        .where(roles: { type: 'Roles::Developer',
+                        state: %i[pending
+                                  profile_completed
+                                  policy_accepted
+                                  screening_completed] } )
+    end
+  end
+end
