8d6bd490c8b1a15d0406bb7bbef671da1ead656d
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-08-26 21:08:07 +0300

    Fix migrations check

--- db/data_schema.rb
+++ db/data_schema.rb
@@ -1,1 +1,2 @@
+# encoding: UTF-8
 DataMigrate::Data.define(version: 20180825143652)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_132700) do
+ActiveRecord::Schema.define(version: 2018_08_26_132700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
