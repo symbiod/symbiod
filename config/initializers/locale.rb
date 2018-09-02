07bd1c81f9e728099d3fcf19af286d37a5d28472
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-09-02 21:27:36 +0300

    Russian localization

---
+++ config/initializers/locale.rb
@@ -0,0 +1,2 @@
+I18n.available_locales = [:en, :ru]
+I18n.default_locale = Rails.env.features? ? :en : :ru
--- spec/helpers/profile_helper_spec.rb
+++ spec/helpers/profile_helper_spec.rb
@@ -6,15 +6,22 @@ describe ProfileHelper do
   subject(:user) { create(:user) }
   subject(:with_assignment_completed) { create(:user, :with_assignment_completed) }
 
-  describe 'human_date' do
-    it { expect(human_date(user)).to eq user.created_at.strftime('%d-%m-%Y') }
+  describe '.human_date' do
+    specify { expect(human_date(user)).to eq user.created_at.strftime('%d-%m-%Y') }
   end
 
-  describe 'github_link' do
-    it {
+  describe '.github_link' do
+    specify do
       expect(github_link(user))
         .to eq "<a target=\"_blank\" href=\"https://github.com/#{user_github}\">#{user_github}</a>"
-    }
+    end
+  end
+
+  describe '.new_member_role_options' do
+    specify do
+      expect(new_member_role_options)
+        .to eq [[t('bootcamp.profile.roles.member'), 'member'], [t('bootcamp.profile.roles.mentor'), 'mentor']]
+    end
   end
 
   def user_github
