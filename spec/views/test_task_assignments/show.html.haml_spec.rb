e4566a3e4fbdc42e52f06e8dcecd79a5a383a9a8
Alexandr Kalinka <alex.kalinoff@gmail.com>
2018-09-05 13:20:41 +0300

    Style changes

--- spec/views/test_task_assignments/show.html.haml_spec.rb
+++ spec/views/test_task_assignments/show.html.haml_spec.rb
@@ -3,19 +3,21 @@
 require 'rails_helper'
 
 describe 'web/dashboard/test_task_assignments/show' do
-  let!(:candidate) { create(:user,
-       :member,
-       :with_assignment_completed,
-       :active,
-       :with_primary_skill_java,
-       first_name: 'Bob',
-       last_name: 'Marley') }
+  let!(:candidate) do
+    create(:user,
+           :member,
+           :with_assignment_completed,
+           :active,
+           :with_primary_skill_java,
+           first_name: 'Bob',
+           last_name: 'Marley')
+  end
 
   before do
     @candidate = candidate
   end
 
-  describe "sss" do
+  describe 'show view' do
     it 'renders without error' do
       expect { render }.not_to raise_error
     end
