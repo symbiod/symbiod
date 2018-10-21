bf08feb76bb1bf55b8e670d1de73125aba449a83
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-10-21 19:02:42 +0300

    Fix specs

--- spec/operations/ops/member/onboarding/send_survey_followup_spec.rb
+++ spec/operations/ops/member/onboarding/send_survey_followup_spec.rb
@@ -7,26 +7,22 @@ describe Ops::Member::Onboarding::SendSurveyFollowup do
 
   describe '#call' do
     let!(:role) { create(:role, :member, :active, :registered_3_days_ago) }
+    before { role.user.create_member_onboarding }
 
     it 'sends email to users with uncomplete survey' do
-      # binding.pry
       expect { subject.call }
         .to have_enqueued_job(ActionMailer::DeliveryJob)
-        .with('Member::UnfinishedSurveyMailer', 'notify', 'deliver_later', role.id)
+        .with('Member::UnfinishedSurveyMailer', 'notify', 'deliver_now', role.id)
     end
 
     it 'updates followup conter' do
-      # binding.pry
-      role.reload
       expect { subject.call }
-        .to change { role.unfinished_survey_followup_counter }.by(1)
+        .to change { role.reload.unfinished_survey_followup_counter }.by(1)
     end
 
     it 'updates last followup date' do
-      # binding.pry
-      role.reload
-      expect { subject }
-        .to change { role.last_unfinished_survey_followup_date }
+      expect { subject.call }
+        .to change { role.reload.last_unfinished_survey_followup_date }
     end
   end
 end
