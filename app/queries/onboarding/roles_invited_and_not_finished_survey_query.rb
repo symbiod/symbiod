a8388982d5a46e832a42153b67ef6fe0ec8f6209
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2018-11-02 20:46:01 +0100

    Fixing tests and PR comments. WIP

--- app/queries/onboarding/roles_invited_and_not_finished_survey_query.rb
+++ app/queries/onboarding/roles_invited_and_not_finished_survey_query.rb
@@ -3,14 +3,13 @@
 module Onboarding
   # Find users where invited and finished survey
   class RolesInvitedAndNotFinishedSurveyQuery
-
     FEEDBACK_STATUS = 'feedback_pending'
 
     def self.call
       Role.select(:user_id)
-        .joins(user: :member_onboarding)
-        .group(:user_id)
-        .where(member_onboardings: { feedback_status: FEEDBACK_STATUS })
+          .joins(user: :member_onboarding)
+          .group(:user_id)
+          .where(member_onboardings: { feedback_status: FEEDBACK_STATUS })
     end
   end
 end
--- spec/jobs/member/onboarding/send_followup_survey_job_spec.rb
+++ spec/jobs/member/onboarding/send_followup_survey_job_spec.rb
@@ -4,13 +4,11 @@ require 'rails_helper'
 
 describe Member::Onboarding::SendFollowupSurveyJob do
   subject { described_class }
-  # let!(:user) { create(:user, :member, :active) }
   let(:role) { create(:role) }
   before { role.user.create_member_onboarding }
 
   describe '#perform' do
     it 'calls Member::Onboarding::SendSurveyFollowup operation' do
-      binding.pry
       expect(Ops::Member::Onboarding::SendSurveyFollowup).to receive(:call).with(role: role)
       subject.perform_now
     end
