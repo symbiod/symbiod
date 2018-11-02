3fa4ecfeb594e98757096d2e7c77784208116a3a
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2018-11-02 21:43:53 +0100

    Tests fixed

--- spec/jobs/member/onboarding/send_followup_survey_job_spec.rb
+++ spec/jobs/member/onboarding/send_followup_survey_job_spec.rb
@@ -8,8 +8,19 @@ describe Member::Onboarding::SendFollowupSurveyJob do
   before { role.user.create_member_onboarding }
 
   describe '#perform' do
+    it 'calls roles query' do
+      expect(::Onboarding::RolesInvitedAndNotFinishedSurveyQuery)
+        .to receive(:call).and_return([role])
+      allow(Ops::Member::Onboarding::SendSurveyFollowup)
+        .to receive(:call).with(role: role)
+      subject.perform_now
+    end
+
     it 'calls Member::Onboarding::SendSurveyFollowup operation' do
-      expect(Ops::Member::Onboarding::SendSurveyFollowup).to receive(:call).with(role: role)
+      allow(::Onboarding::RolesInvitedAndNotFinishedSurveyQuery)
+        .to receive(:call).and_return([role])
+      expect(Ops::Member::Onboarding::SendSurveyFollowup)
+        .to receive(:call).with(role: role)
       subject.perform_now
     end
   end
